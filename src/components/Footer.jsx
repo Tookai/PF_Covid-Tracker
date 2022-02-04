363aaefbfaf9df3a45d60b52875103086d0f225e
Thibaut <tybo.orcel@gmail.com>
2022-02-04 17:17:29 +0100

    open in new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <div className="footer">
       <span>&#169; All Right Reserved</span>
       <p>
-        Web App Author : <a href="https://github.com/Tookai">Thibaut Orcel</a>
+        Web App Author : <a href="https://github.com/Tookai" target="_blank">Thibaut Orcel</a>
       </p>
       <p>
-        API Author : <a href="https://github.com/florianzemma/CoronavirusAPI-France">Florian Zemma Gailleton</a>
+        API Author : <a href="https://github.com/florianzemma/CoronavirusAPI-France" target="_blank">Florian Zemma Gailleton</a>
       </p>
     </div>
   );
